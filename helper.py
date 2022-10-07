eb36440d228112e30530497225809f44558e6f82
Gabriel Ramos <gabrielramos@Gabriels-MacBook-Pro.local>
2022-10-06 19:57:33 -0700

    Gabe's push

--- helper.py
+++ helper.py
@@ -1,53 +1,23 @@
-import pygame
+import pygame, os
 pygame.font.init()
-screen_width = 600
-screen_height = 600
-# Cookie monster Animation
-class Player(pygame.sprite.Sprite):
-	def __init__(self, pos_x, pos_y):
+screen_width = 1400
+screen_height = 900
+screen = pygame.display.set_mode((screen_width,screen_height), pygame.RESIZABLE)
+
+
+class Animations(pygame.sprite.Sprite):
+	def __init__(self, pos_x, pos_y, directory):
 		super().__init__()
 		self.attack_animation = False
 		self.sprites = []
-		self.sprites.append(pygame.image.load('Animations/17aa-0.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-1.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-2.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-3.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-4.jpg')) 
-		self.sprites.append(pygame.image.load('Animations/17aa-5.jpg')) 
-		self.sprites.append(pygame.image.load('Animations/17aa-6.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-7.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-8.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-9.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-10.jpg')) 
-		self.sprites.append(pygame.image.load('Animations/17aa-11.jpg')) 
-		self.sprites.append(pygame.image.load('Animations/17aa-12.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-13.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-14.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-15.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-16.jpg')) 
-		self.sprites.append(pygame.image.load('Animations/17aa-17.jpg')) 
-		self.sprites.append(pygame.image.load('Animations/17aa-18.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-19.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-20.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-21.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-22.jpg')) 
-		self.sprites.append(pygame.image.load('Animations/17aa-23.jpg')) 
-		self.sprites.append(pygame.image.load('Animations/17aa-24.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-25.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-26.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-27.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-28.jpg')) 
-		self.sprites.append(pygame.image.load('Animations/17aa-29.jpg')) 
-		self.sprites.append(pygame.image.load('Animations/17aa-30.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-31.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-32.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-33.jpg'))
-		self.sprites.append(pygame.image.load('Animations/17aa-34.jpg')) 
-		self.sprites.append(pygame.image.load('Animations/17aa-35.jpg')) 
-		self.sprites.append(pygame.image.load('Animations/17aa-36.jpg'))        
+		list = os.listdir(directory)
+		list.sort()
 
-
-
+		for item in list[1:]:
+			if directory == "Animations/Background":
+				self.sprites.append(pygame.transform.scale(pygame.image.load(directory + '/' + item), (1400,900)))
+			else:
+				self.sprites.append(pygame.image.load(directory + '/' + item))
 
 		self.current_sprite = 0
 		self.image = self.sprites[self.current_sprite]
@@ -55,7 +25,7 @@ class Player(pygame.sprite.Sprite):
 		self.rect = self.image.get_rect()
 		self.rect.topleft = [pos_x,pos_y]
 
-	def attack(self):
+	def idle(self):
 		self.attack_animation = True
 
 	def update(self,speed):
@@ -66,8 +36,6 @@ class Player(pygame.sprite.Sprite):
 				self.attack_animation = False
 
 		self.image = self.sprites[int(self.current_sprite)]
-
-
 
 font = pygame.font.SysFont('georgia', 20)
 displayStartText = font.render("START SCREEN", False, (0,0,0))
@@ -100,5 +68,7 @@ class Button:
 				return True
 		return False
 
-startText = Button((255,255,255),screen_height/2, screen_width/2, 100, 25, "Start")
-settingText = Button((255,255,255),screen_height/2 + 50, screen_width/2 + 50, 100, 25, "Settings")
+startText = Button((255,255,255),screen_width/2, screen_height/2, 100, 25, "Start")
+settingText = Button((255,255,255),screen_width/2, screen_height/2 + 50, 100, 25, "Settings")
+returnText = Button((255,255,0), 1200, 100, 100, 25, "Return")
+quitText = Button((255,255,255),screen_width/2, screen_height/2 + 100, 100, 25, "Quit")
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pygame, sys
+from time import sleep
+import pygame, sys, os
 from helper import *
 #samee
 #gabe
@@ -16,14 +17,24 @@ pygame.font.init()
 
 
 # Game Screen
-screen_width = 600
-screen_height = 600
+screen_width = 1400
+screen_height = 900
 screen = pygame.display.set_mode((screen_width,screen_height), pygame.RESIZABLE)
 
 # Creating the sprites and groups
 moving_sprites = pygame.sprite.Group()
-player = Player(100,100)
-moving_sprites.add(player)
+sleepFox = Animations(0, 0, 'Animations/SleepFoxAnimation')
+goldieSit = Animations(600, 0, 'Animations/GoldieAnimation')
+silverSit = Animations(0, 350, 'Animations/SilverSitAnimation')
+catRun = Animations(600, 350, 'Animations/CatRunAnimation')
+moving_sprites.add(sleepFox)
+moving_sprites.add(goldieSit)
+moving_sprites.add(silverSit)
+moving_sprites.add(catRun)
+
+startScreen = pygame.sprite.Group()
+background = Animations(0,0, 'Animations/Background')
+startScreen.add(background)
 
 # Start Screen
 start = True
@@ -31,6 +42,13 @@ start = True
 # Setting Screen
 setting = False
 displaySettingText = font.render("SETTINGS", False, (0,0,0))
+
+# Choosing Character Screen
+choosing = False
+displayChoosingText = font.render("CHOOSE YOUR CHARACTER", False, (0,0,0))
+
+# WIP
+displayyWIP = font.render("WORK IN PROGRESS", False, (255,255,255))
 
 while True:
 	for event in pygame.event.get():
@@ -41,8 +59,6 @@ while True:
 			if event.key == pygame.K_ESCAPE:
 				pygame.quit()
 				sys.exit()
-			if event.key == pygame.K_0:
-				player.attack()
 			
     
 	while (start):
@@ -55,25 +71,34 @@ while True:
 				if event.key == pygame.K_ESCAPE:
 					pygame.quit()
 					sys.exit()
-				if event.key == pygame.K_0:
-					start = False
 			if event.type == pygame.MOUSEBUTTONDOWN:
 				if startText.isOver(pos):
 					print("Start is pressed")
 					start = False
+					choosing = True
 				if settingText.isOver(pos):
 					print("Settings is pressed")
 					setting = True
 					start = False
+				if quitText.isOver(pos):
+					print("Quit is pressed")
+					pygame.quit()
+					sys.exit()
 
 
 		screen.fill((255,255,255))
+		startScreen.draw(screen)
+		startScreen.update(0.15)
+		background.idle()
 		startText.draw(screen, (255,255,255))
 		settingText.draw(screen, (255,255,255))
+		quitText.draw(screen, (255,255,255))
+		
 		pygame.display.flip()
 
 	while (setting):
 		for event in pygame.event.get():
+			pos = pygame.mouse.get_pos()
 			if event.type == pygame.QUIT:
 				pygame.quit()
 				sys.exit()
@@ -84,18 +109,44 @@ while True:
 				if event.key == pygame.K_0:
 					start = True
 					setting = False
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				if returnText.isOver(pos):
+					print("Return is pressed")
+					setting = False
+					start = True
 			
-		screen.fill((255,255,255))
-		screen.blit(displaySettingText,(100,100))
+		screen.fill((255,255,0))
+		screen.blit(displaySettingText,(screen_width/2 - displaySettingText.get_width()/2, 50))
+		returnText.draw(screen,(255,255,0))
 		pygame.display.flip()
 
-
+	while(choosing):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_0:
+					setting = True
+					choosing = False
+				if event.key == pygame.K_1:
+					choosing = False
+		
+		screen.fill((0,0,255))
+		screen.blit(displayChoosingText, (screen_width/2 - displayChoosingText.get_width()/2,50))
+		moving_sprites.draw(screen)
+		moving_sprites.update(0.05)
+		sleepFox.idle()
+		goldieSit.idle()
+		silverSit.idle()
+		catRun.idle()
+		pygame.display.flip()
+		clock.tick(240)
 
     # Drawing
-	screen.fill((0,0,0)) 
-	moving_sprites.draw(screen)
-	moving_sprites.update(0.25)
 	pygame.display.flip()
+	screen.fill((0,0,0))
+	screen.blit(displayyWIP, ((screen_width/2) - (displayChoosingText.get_width()/2), (screen_height/2) - (displayChoosingText.get_height()/2)))
 	clock.tick(240)
 
 pygame.quit()
